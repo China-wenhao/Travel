784a55ce7ad4d08e22517d6ec10b536df1765c48
China-wenhao <wen1746594617@foxmail.com>
2022-08-22 12:43:39 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
