346f046cfddfcff64c10a079c8bb98501ad780e9
wenhao <1746594617@qq.com>
2022-08-23 14:36:35 +0800

    update Swiper.vue 2022/8/23

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/style/reset.css'
 import './assets/style/border.css'
 import './assets/style/iconfont.css'
 
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   render: h => h(App),
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
         <home-header></home-header>
+        <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header';
-    export default {
-        name:"Home-home",
-        components:{
-            HomeHeader
-        }
+import HomeSwiper from './components/Swiper';
+export default {
+    name: "Home-home",
+    components: {
+        HomeHeader,
+        HomeSwiper
     }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
